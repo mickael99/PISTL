76e17e9aa1e3b32287dd1a39dc57d3e2ee425be7
Daniel SIMA <simadaniel@hotmail.com>
2023-11-27 12:59:18 +0100

    update readme

--- Project/backend/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ Project/backend/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f962fde9ed236ffcccf5ce8e424efdd29fa96e66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Project/readme.md
+++ Project/readme.md
@@ -7,7 +7,13 @@
 > Installation Server (qu'une seule fois, pas besoin je pense sur Windows et peut-être même pour Mac)
 
 ```bash
-sqlcmd -S localhost1 -U sa -P 'votreMotDePasse' -C # mot de passe choisi dans sudo /opt/mssql/bin/mssql-conf setup
+sudo /opt/mssql/bin/mssql-conf setup # choix 3 puis fournir mot de passe
+
+sudo systemctl enable mssql-server
+
+sudo systemctl start mssql-server
+
+sqlcmd -S localhost -U sa -P 'votreMotDePasse' -C # mot de passe choisi dans sudo /opt/mssql/bin/mssql-conf setup
 ```
 
 Sur VS code `Ctrl+Shift+P`-> MS SQL: Manage Connection Profiles et rémplir avec `localhost` et le Login/MotDePasse
