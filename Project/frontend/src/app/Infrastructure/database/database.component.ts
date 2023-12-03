086e88a52d0bde4dbbe3ccbc957acc3438bba8fd
Daniel SIMA <simadaniel@hotmail.com>
2023-12-03 21:52:38 +0100

    adding component Database

---
+++ Project/frontend/src/app/Infrastructure/database/database.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-database',
+  templateUrl: './database.component.html',
+  styleUrls: ['./database.component.css']
+})
+export class DatabaseComponent {
+
+}
--- Project/frontend/src/app/app.module.ts
+++ Project/frontend/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LoginComponent } from './Login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SysAdminByDomainComponent } from './Domain/sys-admin-by-domain/sys-admin-by-domain.component';
 import { DomainBySysAdminComponent } from './Domain/domain-by-sys-admin/domain-by-sys-admin.component';
+import { DatabaseComponent } from './Infrastructure/database/database.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { DomainBySysAdminComponent } from './Domain/domain-by-sys-admin/domain-b
     LoginComponent,
     SysAdminByDomainComponent,
     DomainBySysAdminComponent,
+    DatabaseComponent,
   ],
   imports: [BrowserModule, HttpClientModule, FormsModule, AppRoutingModule],
   providers: [],
