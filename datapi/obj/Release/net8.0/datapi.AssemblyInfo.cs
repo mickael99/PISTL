68b66d78cb9de2b8f78f454e8365e1da0c09bcb4
Yukhoi <yukai_luo@yahoo.com>
2023-12-05 00:30:20 +0100

    interfaces

--- datapi/obj/Release/net8.0/datapi.AssemblyInfo.cs
+++ datapi/obj/Release/net8.0/datapi.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("datapi")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Project")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Project")]
+[assembly: System.Reflection.AssemblyProductAttribute("datapi")]
+[assembly: System.Reflection.AssemblyTitleAttribute("datapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // 由 MSBuild WriteCodeFragment 类生成。
--- datapi/obj/Release/net8.0/datapi.RazorAssemblyInfo.cs
+++ datapi/obj/Release/net8.0/datapi.RazorAssemblyInfo.cs
@@ -11,13 +11,8 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Project")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Project")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ProvideApplicationPartFactoryAttribute("Microsoft.AspNetCore.Mvc.ApplicationParts.ConsolidatedAssemblyApplicationPartFact" +
+    "ory, Microsoft.AspNetCore.Mvc.Razor")]
 
 // 由 MSBuild WriteCodeFragment 类生成。
 
