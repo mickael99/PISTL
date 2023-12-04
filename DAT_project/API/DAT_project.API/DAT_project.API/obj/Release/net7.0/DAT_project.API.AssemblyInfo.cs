cf2c6701f45f4fab449b51a5e80bab0c9fb5a8f2
Yukhoi <yukai_luo@yahoo.com>
2023-12-05 00:22:37 +0100

    Committing local changes

--- DAT_project/API/DAT_project.API/DAT_project.API/obj/Release/net7.0/DAT_project.API.AssemblyInfo.cs
+++ DAT_project/API/DAT_project.API/DAT_project.API/obj/Release/net7.0/DAT_project.API.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("DAT_project.API")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Project")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Project")]
+[assembly: System.Reflection.AssemblyProductAttribute("DAT_project.API")]
+[assembly: System.Reflection.AssemblyTitleAttribute("DAT_project.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // 由 MSBuild WriteCodeFragment 类生成。
--- TestBDD/obj/Release/net7.0/TestBDD.AssemblyInfo.cs
+++ TestBDD/obj/Release/net7.0/TestBDD.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("TestBDD")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Project")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Project")]
+[assembly: System.Reflection.AssemblyProductAttribute("TestBDD")]
+[assembly: System.Reflection.AssemblyTitleAttribute("TestBDD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // 由 MSBuild WriteCodeFragment 类生成。
