4a60f0581e0ce3d9b2588e3fe11a0f8c10cae8ac
21207359 <mickael.temim99@gmail.com>
2023-12-11 19:18:21 +0100

    affichage domaines + debut ajout domaine

--- Project/frontend/src/app/Domain/domain-administration/domain-administration.component.ts
+++ Project/frontend/src/app/Domain/domain-administration/domain-administration.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Renderer2 } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-domain-administration',
@@ -7,13 +8,18 @@ import { Component, Renderer2 } from '@angular/core';
   styleUrls: ['./domain-administration.component.css'],
 })
 export class DomainAdministrationComponent {
-  domains: any;
-  domainName: string = '';
-  createdBy: string = '';
+  domains: any[];
+  name: string = '';
+  logo: string = '';
   edition: string = '';
+  isSsoEnabled: boolean = false;
+  comment: string = '';
+  parentCompany: string = '';
   selectedDomain: any;
 
-  constructor(private renderer: Renderer2, private http: HttpClient) {
+  isNewDomainMode: boolean = false;
+
+  constructor(private renderer: Renderer2, private http: HttpClient, private router: Router) {
     this.http.get('http://localhost:5050/api/domain').subscribe(
       (data: any) => {
         this.domains = data;
@@ -24,24 +30,45 @@ export class DomainAdministrationComponent {
     );
   }
 
-  addDomain() {
-    this.http
-      .post('http://localhost:5050/api/domain', {
-        domainName: this.domainName,
-        createdBy: this.createdBy,
-        edition: this.edition,
-      })
-      .subscribe({
-        next: (data: any) => {
-          this.domains = data;
-        },
-        error: (error: any) => {
-          alert('Connection error: ' + error.message);
-        },
-      });
-  }
-
   onSelect(selectedDomain: any) {
     this.selectedDomain = selectedDomain;
   }
+
+  startNewDomainMode() {
+    console.log("je rentre dans startnewdomain");
+    this.isNewDomainMode = true;
+
+    this.name = '';
+    this.logo = '';
+    this.edition = '';
+    this.isSsoEnabled = false;
+    this.comment = '';
+    this.parentCompany = '';
+  }
+
+  endNewDomainMode() {
+    this.isNewDomainMode = false;
+  }
+
+  addDomain() {
+    if(this.isNewDomainMode) {
+      this.http
+        .post('http://localhost:5050/api/domain', {
+          name: this.name,
+          logo: this.logo,
+          edition: this.edition,
+          isSsoEnabled: this.isSsoEnabled,
+          comment: this.comment,
+          parentCompany: this.parentCompany
+        })
+        .subscribe({
+          next: (data: any) => {
+            this.domains = data;
+          },
+          error: (error: any) => {
+            alert('Connection error: ' + error.message);
+          },
+        });
+      }
+  } 
 }
