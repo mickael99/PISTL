7fb010c3578402413e373c9431ca03628909f447
Yukhoi <yukai_luo@yahoo.com>
2023-12-19 14:02:13 +0100

    docker

---
+++ Project/docker/dbo/Tables/1_Domain.sql
@@ -0,0 +1,66 @@
+﻿--v11, HFU 2019-06-20, TASK 8287,  Add IsSsoEnabled
+
+create table [dbo].[Domain] (
+    [DomainID]           INT             identity (1, 1) not null,
+    [Name]               NVARCHAR (50)   not null,
+    [Logo]               VARBINARY (MAX) null,
+    [Edition]            VARCHAR (MAX)   null,
+    [IsSsoEnabled]       BIT             constraint [DF_Domain_IsSSO] default ((0)) not null,
+    [Comment]            NVARCHAR (MAX)  null,
+    [ParentCompany]      NVARCHAR (50)   null,
+    [CreatedDate]        DATETIME        constraint [DF_Domain_CreatedDate] default (getdate()) null,
+    [CreatedBy]          NVARCHAR (200)  null,
+    [ModifiedDate]       DATETIME        constraint [DF_Domain_ModifiedDate] default (getdate()) null,
+    [ModifiedBy]         NVARCHAR (200)  null,
+    constraint [PK_Domain] primary key clustered ([DomainID] asc)
+);
+GO
+
+
+create trigger [dbo].[tr_Domain_Delete]
+on [dbo].[Domain]
+after delete
+as
+begin
+
+	set nocount on
+
+	insert Domain_Log (LogAction, LogDate, DomainID, Name, Logo, Edition, IsSsoEnabled, Comment, CreatedDate, CreatedBy, ModifiedDate, ModifiedBy)
+	select 'D', getdate(), DomainID, Name, Logo, Edition, IsSsoEnabled, Comment, CreatedDate, CreatedBy, ModifiedDate, ModifiedBy
+	from   deleted
+end
+GO
+
+create trigger [dbo].[tr_Domain_Insert]
+on [dbo].[Domain]
+after insert
+as
+begin
+
+	set nocount on
+
+	insert Domain_Log (LogAction, LogDate, DomainID, Name, Logo, Edition, IsSsoEnabled, Comment, CreatedDate, CreatedBy, ModifiedDate, ModifiedBy)
+	select 'I', getdate(), DomainID, Name, Logo, Edition, IsSsoEnabled, Comment, CreatedDate, CreatedBy, ModifiedDate, ModifiedBy
+	from   inserted
+
+end
+GO
+
+create trigger [dbo].[tr_Domain_Update]
+on [dbo].[Domain]
+after update
+as
+begin
+
+	set nocount on
+
+	insert Domain_Log (LogAction, LogDate, DomainID, Name, Logo, Edition, IsSsoEnabled, Comment, CreatedDate, CreatedBy, ModifiedDate, ModifiedBy)
+	select 'V', getdate(), DomainID, Name, Logo, Edition, IsSsoEnabled, Comment, CreatedDate, CreatedBy, ModifiedDate, ModifiedBy
+	from   deleted
+
+	insert Domain_Log (LogAction, LogDate, DomainID, Name, Logo, Edition, IsSsoEnabled, Comment, CreatedDate, CreatedBy, ModifiedDate, ModifiedBy)
+	select 'U', getdate(), DomainID, Name, Logo, Edition, IsSsoEnabled, Comment, CreatedDate, CreatedBy, ModifiedDate, ModifiedBy
+	from   inserted
+
+end
+GO
---
+++ Project/docker/dbo/Tables/LoginPasswordRule.sql
@@ -0,0 +1,8 @@
+﻿create table [dbo].[LoginPasswordRule] (
+    [PasswordRuleID]      UNIQUEIDENTIFIER not null,
+    [DomainID]            INT              not null,
+    [InternalDescription] VARCHAR (510)    not null,
+    [DictionaryItemCode]  NVARCHAR (100)   not null,
+    [Pattern]             NVARCHAR (MAX)   not null
+);
+
